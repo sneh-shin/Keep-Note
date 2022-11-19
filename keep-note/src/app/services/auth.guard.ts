667f440311a30841b8496ec9abe6f0d58e70d9f9
sneh-shin <snehashinde800@gmail.com>
2022-11-19 21:56:29 +0530

    added canActivate() in auth.guard

--- keep-note/src/app/services/auth.guard.ts
+++ keep-note/src/app/services/auth.guard.ts
@@ -14,6 +14,10 @@ export class AuthGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (!this.auth.isLoggedIn) {
+      this.route.navigateToLoginView();
+      return false;
+    }
     return true;
   }
   
